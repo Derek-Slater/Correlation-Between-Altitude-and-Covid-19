c4da348be7de19b81a96059ac070f74569cbf59d
Derek Slater <derekslater01@gmail.com>
2022-05-03 17:49:47 -0700

    Removed an import

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import country_converter as coco #pip install country-converter
 import pycountry
 from seaborn import clustermap
 
